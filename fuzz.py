237d578ab9526b75668c2a7b6a28630760a9448b
hzc0104 <164685184+hzc0104@users.noreply.github.com>
2024-04-25 17:56:02 -0500

    Update fuzz.py

--- fuzz.py
+++ fuzz.py
@@ -1,127 +1,74 @@
-import os
-
 import subprocess
-
+import os
 import shutil
-
 import time
-
 from datetime import datetime
 
-# Method 1: Fuzz giveTimeStamp() Method
-
+#This method is fuzzing for a timestamp that follows along with generic time display.
 def fuzz_giveTimeStamp():
-
     def giveTimeStamp():
-
-        tsObj = time.time()
-
-        strToret = datetime.fromtimestamp(tsObj).strftime('%Y-%m-%d %H:%M:%S')
-
-        return strToret
- 
+        ts = time.time()
+        str = datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        return str
     print(giveTimeStamp())
- 
-# Method 2: Fuzz deleteRepo() Method
 
+#This method is fuzzing the deletion of a repos whether done or not. 
 def fuzz_deleteRepo():
-
-    def deleteRepo(dirName, type_):
-
-        print(':::' + type_ + ':::Deleting ', dirName)
-
+    def deleteRepo(dirName, typeA):
+        print(':::' + typeA+ ':::Deleting ', dirName)
         try:
-
             if os.path.exists(dirName):
-
                 shutil.rmtree(dirName)
-
-                print("Directory", dirName, "deleted successfully.")
-
+                print("Directory", dirName, "is deleted now.")
             else:
-
-                print("Directory", dirName, "does not exist.")
-
+                print("Directory", dirName, "isn't here y'all.")
         except Exception as e:
-
-            print("Failed deleting directory:", e)
+            print("Failure in deleting directory:", e)
  
     fuzz_dirName = "test_directory"
-
-    fuzz_type = "TEST_TYPE"
-
-    deleteRepo(fuzz_dirName, fuzz_type)
+    fuzz_typeA = "test_type"
+    deleteRepo(fuzz_dirName, fuzz_typeA)
  
-# Method 3: Fuzz dumpContentIntoFile() Method
+#This third method is related to whether the content was actually put into the file or not.
 
 def fuzz_dumpContentIntoFile():
-
-    def dumpContentIntoFile(strP, fileP):
-
-        fileToWrite = open(fileP, 'w')
-
-        fileToWrite.write(strP)
-
+    def dumpContentIntoFile(sP, fP):
+        fileToWrite = open(fP, 'w')
+        fileToWrite.write(sP)
         fileToWrite.close()
-
-        return str(os.stat(fileP).st_size)
+        return str(os.stat(fP).st_size)
+    fuzz_sP = "Testing if it worked for content to write into file."
+    fuzz_fP = "test_file.txt"
+    print("Once writte, your file size is:", dumpContentIntoFile(fuzz_sP, fuzz_fP))
  
-    fuzz_strP = "Test content to write into file."
-
-    fuzz_fileP = "test_file.txt"
-
-    print("File size after writing:", dumpContentIntoFile(fuzz_strP, fuzz_fileP))
- 
-# Method 4: Fuzz makeChunks() Method
-
+# This fourth methid is fuzzing the makeChunks method
 def fuzz_makeChunks():
-
-    def makeChunks(the_list, size_):
-
-        for i in range(0, len(the_list), size_):
-
-            yield the_list[i:i + size_]
- 
+    def makeChunks(aList, sizeA):
+        for i in range(0, len(aList), sizeA):
+            yield aList[i:i + sizeA]
     fuzz_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
     fuzz_size = 3
-
     for chunk in makeChunks(fuzz_list, fuzz_size):
-
         print(chunk)
  
-# Method 5: Fuzz cloneRepo() Method
-
+# This fifth method is just fuzzing on the method for the cloneRepo 
 def fuzz_cloneRepo():
-
-    def cloneRepo(repo_name, target_dir):
-
-        cmd_ = "git clone " + repo_name + " " + target_dir
-
+    def cloneRepo(repoName, targetDir):
+        cmdA = "git clone " + repoName + " " + targetDir
         try:
-
-            subprocess.check_output(['bash', '-c', cmd_])
-
-            print("Repository cloned successfully:", repo_name)
-
+            subprocess.check_output(['bash', '-c', cmdA])
+            print("Repository cloned successfully:", repoName)
         except subprocess.CalledProcessError:
-
-            print('Skipping this repo ... trouble cloning repo:', repo_name)
+            print('Skipping this repo ... trouble cloning repo:', repoName)
  
-    fuzz_repo_name = "https://github.com/username/repository.git"
+    fuzz_repoName = "https://github.com/username/repository.git"
+    fuzz_targetDir = "/path/to/target_directory"
 
-    fuzz_target_dir = "/path/to/target_directory"
-
-    cloneRepo(fuzz_repo_name, fuzz_target_dir)
+    cloneRepo(fuzz_repoName, fuzz_targetDir)
  
 if __name__ == "__main__":
-
     fuzz_giveTimeStamp()
-
     fuzz_deleteRepo()
-
     fuzz_dumpContentIntoFile()
-
     fuzz_makeChunks()
-
     fuzz_cloneRepo()
